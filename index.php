0feed50d93b8a4bcd1abf2147637628dc289e4af
arianafc <arianafc1108@gmail.com>
2025-03-02 01:22:14 -0600

    update

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Los Jaules - Club Campestre</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="./css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Averia+Serif+Libre&display=swap" rel="stylesheet">
+</head>
+
+<body>
+<header>
+        <div class="top-bar ">
+            <p>¡Unite a nosotros y aprovechá nuestro Pase del Día!</p>
+        </div>
+        <nav class="navbar">
+            <div class="logo">Los Jaúles - CLUB CAMPESTRE</div> 
+            <ul class="nav-links">
+                <li><a href="#">Servicios</a></li>
+                <li><a href="#">Los Jaúles</a></li>
+                <li><a href="#">GYM</a></li>
+                <li><a href="#">Actividades</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Horarios y contacto</a></li>
+            </ul>
+            <button class="btn-membresia">Iniciar Sesión</button>
+        </nav>
+    </header>
+    <section class="hero">
+        <div class="hero-content">
+            <h1>Bienvenido a Club Los Jaúles</h1>
+            <p>Disfruta de la naturaleza, el deporte y la relajación.</p>
+            <button class="btn-pase">Pase del Día</button>
+        </div>
+    </section>
+</body>
+
+</html>
